831370ac6a71076695ad3ee1e22a9a22bdb8b7f4
mbisurgi-designfreed <maximiliano@designfreed.com>
2017-11-07 01:24:41 -0300

    Cambiar direccion server produccion

--- src/actions/auth.action.js
+++ src/actions/auth.action.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:4000';
+const API_URL = 'https://galias-server-api.herokuapp.com';
 
 export const signin = ({ email, password }, history) => {
     return async (dispatch) => {
