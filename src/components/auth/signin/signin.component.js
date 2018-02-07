cb04bebe0dd93345a1fc78948ffc10f7043a57fb
mbisurgi-designfreed <maximiliano@designfreed.com>
2018-02-06 21:03:27 -0300

    Agregar login nuevo

--- src/components/auth/signin/signin.component.js
+++ src/components/auth/signin/signin.component.js
@@ -1,66 +1,59 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { reduxForm, Field } from 'redux-form';
+import { withFormik, Form, Field } from 'formik';
 
 import { signin } from '../../../actions/auth.action';
 
 class Signin extends Component {
-    renderField({ input, label, type, meta: { touched, error, valid } }) {
-        return (
-            <div className="form-group">
-                <label>{label}</label>
-                <input {...input} className={`form-control ${touched && !valid ? 'is-invalid' : ''}`} type={type} />
-                {touched && error && <span className="invalid-feedback">{error}</span>}
-            </div>
-        );
-    }
+    // renderError() {
+    //     const { errorMessage } = this.props;
 
-    renderError() {
-        const { errorMessage } = this.props;
-
-        if (this.props.errorMessage) {
-            return (
-                <div className="alert alert-danger text-center">{errorMessage}</div>
-            );
-        }
-    }
+    //     if (this.props.errorMessage) {
+    //         return (
+    //             <div className="alert alert-danger text-center">{errorMessage}</div>
+    //         );
+    //     }
+    // }
 
     onIngresar(values) {
         this.props.signin(values, this.props.history);
     }
 
     render() {
-        const { handleSubmit, valid } = this.props;
-
         return (
-            <div className="row justify-content-center mt-5">
-                <form className="col-md-4" onSubmit={handleSubmit(this.onIngresar.bind(this))} noValidate >
-                    <Field name="email" component={this.renderField} type="email" label="Email" />
-                    <Field name="password" component={this.renderField} type="password" label="Contraseña" />
-                    {this.renderError()}
-                    <button type="submit" className="btn btn-danger btn-block" disabled={!valid}>Ingresar</button>
-                </form>
-            </div>
+            <Form className="login">
+                <div className="form-group">
+                    <label className="form__label" htmlFor="email">Email</label>
+                    <Field className="form__field" id="email" type="email" name="email" />
+                </div>
+                <div className="form-group">
+                    <label className="form__label" htmlFor="password">Password</label>
+                    <Field className="form__field" id="password" type="password" name="password" />
+                </div>
+                <div className="row text-center">
+                    <button className="btn mt-small" >Ingresar</button>
+                </div>
+            </Form>
         );
     }
 }
 
-const validate = (values) => {
-    const errors = {};
+const onSubmit = (values, { props, resetForm }) => {
+    const { email, password } = values;
 
-    if (!values.email) {
-        errors.email = 'Email es requerido'
-    }
+    props.signin({ email, password }, props.history);
+};
 
-    if (!values.password) {
-        errors.password = 'Contraseña es requerido'
-    }
-
-    return errors;
-}
+const mapPropsToValues = (props) => ({
+    email: '',
+    password: ''
+});
 
 const mapStateToProps = (state) => {
     return { errorMessage: state.auth.error };
 }
 
-export default reduxForm({ form: 'signin', validate })(connect(mapStateToProps, { signin })(Signin));
+export default connect(mapStateToProps, { signin })(withFormik({
+    mapPropsToValues,
+    handleSubmit: onSubmit
+})(Signin));
